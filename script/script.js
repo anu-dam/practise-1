b1b396db5dd45ee4c01fcacff5245cbdd49536a1
Anuradha Dambaladeniya <anuradhadambaladeniya@Anuradhas-MacBook-Pro.local>
2019-12-02 22:43:43 +1030

    allignment adjustments

--- script/script.js
+++ script/script.js
@@ -1,28 +1,28 @@
 
-$(".tab-panels .tabs li").on("click", function(){
+$(".tab-panels .tabs li").on("click", function () {
     $(".tab-panels .tabs li").removeClass("active");
     $(this).addClass("active");
 
-// figure out which panel to show
+    // figure out which panel to show
 
-var showPanel = $(this).attr("rel");
+    var showPanel = $(this).attr("rel");
 
 
-// hide current panel 
+    // hide current panel 
 
-$('.panel.active').slideUp(300, showNext); 
-    
-    
+    $('.panel.active').slideUp(300, showNext);
+
+
     function showNext() {
-    $(this).removeClass("active");
-    $("#" + showPanel).slideDown(300,function(){
-        $(this).addClass("active");
-    });
+        $(this).removeClass("active");
+        $("#" + showPanel).slideDown(300, function () {
+            $(this).addClass("active");
+        });
 
-};
+    };
 
 
-// show new panel
+    // show new panel
 
 
 
